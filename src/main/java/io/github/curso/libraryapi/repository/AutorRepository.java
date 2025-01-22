ed1424b33b9825aa9374db50c3eb82b00d8551f1
macsantoGIT <marcelo.correa.santos@gmail.com>
2025-01-22 17:53:36 -0300

    Iniciando projeto

--- src/main/java/io/github/curso/libraryapi/repository/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.curso.libraryapi.repository;
-
-import io.github.curso.libraryapi.model.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AutorRepository extends JpaRepository<Autor, UUID> {
-}
