cf188e0fd6940a00391e472dda310ff6eb4ab02d
macsantosGIT <marcelo.correa.santos@gmail.com>
2025-05-19 19:29:50 -0300

    Seção 7 - Aulas de 106 a 110

--- libraryapi/src/main/java/io/github/cursospring/libraryapi/controller/LivroController.java
+++ libraryapi/src/main/java/io/github/cursospring/libraryapi/controller/LivroController.java
@@ -3,14 +3,17 @@ package io.github.cursospring.libraryapi.controller;
 import io.github.cursospring.libraryapi.controller.dto.CadastroLivroDTO;
 import io.github.cursospring.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import io.github.cursospring.libraryapi.controller.mappers.LivroMapper;
+import io.github.cursospring.libraryapi.model.GeneroLivro;
 import io.github.cursospring.libraryapi.model.Livro;
 import io.github.cursospring.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/livros")
@@ -51,5 +54,47 @@ public class LivroController implements GenericController {
                    return ResponseEntity.noContent().build();
                 }).orElseGet( () -> ResponseEntity.notFound().build());
     }
+
+    @GetMapping
+    public ResponseEntity<List<ResultadoPesquisaLivroDTO>> pesquisa(
+            @RequestParam(value ="isbn", required = false)
+            String isbn,
+            @RequestParam(value ="titulo", required = false)
+            String titulo,
+            @RequestParam(value ="nome-autor", required = false)
+            String nomeAutor,
+            @RequestParam(value ="genero", required = false)
+            GeneroLivro genero,
+            @RequestParam(value ="ano-publicacao", required = false)
+            Integer anoPuclicacao
+    ){
+        var resultado = service.pesquisa(isbn, titulo, nomeAutor, genero, anoPuclicacao);
+        var lista = resultado.
+                stream().
+                map(mapper::toDTO).
+                collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> atualizar(
+            @PathVariable("id") String id, @RequestBody @Valid CadastroLivroDTO dto){
+
+        return service.obterPorId(UUID.fromString(id))
+                .map(livro -> {
+                    Livro entidadeAuxiliar = mapper.toEntity(dto);
+                    livro.setDataPublicacao(entidadeAuxiliar.getDataPublicacao());
+                    livro.setIsbn(entidadeAuxiliar.getIsbn());
+                    livro.setPreco(entidadeAuxiliar.getPreco());
+                    livro.setGenero(entidadeAuxiliar.getGenero());
+                    livro.setTitulo(entidadeAuxiliar.getTitulo());
+                    livro.setAutor(entidadeAuxiliar.getAutor());
+
+                    service.atualizar(livro);
+
+                    return ResponseEntity.noContent().build();
+                }).orElseGet( () -> ResponseEntity.notFound().build());
+    }
+
 }
 
--- libraryapi/src/main/java/io/github/cursospring/libraryapi/service/LivroService.java
+++ libraryapi/src/main/java/io/github/cursospring/libraryapi/service/LivroService.java
@@ -1,12 +1,19 @@
 package io.github.cursospring.libraryapi.service;
 
+import io.github.cursospring.libraryapi.model.GeneroLivro;
 import io.github.cursospring.libraryapi.model.Livro;
 import io.github.cursospring.libraryapi.repository.LivroRepository;
+import io.github.cursospring.libraryapi.repository.specs.LivroSpecs;
 import io.github.cursospring.libraryapi.validator.LivroValidator;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.LinkStyle;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static io.github.cursospring.libraryapi.repository.specs.LivroSpecs.*;
 
 @Service
 public class LivroService {
@@ -32,5 +39,33 @@ public class LivroService {
         repository.delete(livro);
     }
 
+    public List<Livro> pesquisa(
+            String isbn, String titulo, String nomeAutor,
+            GeneroLivro genero, Integer anoPublicacao){
+        // where 0=0 - retorno uma clausula verdadeira
+        Specification<Livro> specs =
+                Specification.where(((root, query,
+                                      cb) -> cb.conjunction()));
+        if(isbn != null)
+            specs = specs.and(isbnEqual(isbn));
+        if(titulo != null)
+            specs = specs.and(tituloLike(titulo));
+        if(genero != null)
+            specs = specs.and(generoEqual(genero));
+        if(anoPublicacao != null)
+            specs = specs.and(anoPublicacaoEqual(anoPublicacao));
+        if(nomeAutor != null)
+            specs = specs.and(nomeAutorLike(nomeAutor));
 
+        return repository.findAll(specs);
+    }
+
+
+    public void atualizar(Livro livro) {
+        if(livro.getId() == null){
+            throw new IllegalArgumentException("Para atualizar é necessaŕio que o livro esteja cadastrado");
+        }
+
+        repository.save(livro);
+    }
 }
